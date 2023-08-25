17c1faf78be555cf503d893611e08bb760b9ff90
pedro martinez <pedromartinez_goyo@hotmail.com>
2023-08-24 23:28:42 -0500

    init: created project. clean up

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Expenses tracker</title>
+        <meta name="description" content="App for managing your monthly expenses" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${inter.className}`}>
+        <h1>Expenses tracker</h1>
+      </main>
+    </>
+  )
+}
